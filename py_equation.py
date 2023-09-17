c3fa617b99c9b7c4459d91b7bbfad8ebcd8ae2f6
Rohan saeed <109614066+rohansaeed2010@users.noreply.github.com>
2023-09-17 13:59:11 +0500

    Update py_equation.py
    show Linear and Quadratic graphs

--- py_equation.py
+++ py_equation.py
@@ -1,9 +1,49 @@
-x = 5
-y = x**3+6-8*5
-print(y)
-msg = "hello world"
-print(msg)
-z = y+x**2+1
-print(z)
-msg2 = "EQUATION # 2"
-print(msg2)
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Linear Equation: y = mx + b
+def linear_equation(x):
+    m = 2  # Slope
+    b = 3  # Y-intercept
+    return m * x + b
+
+# Quadratic Equation: y = ax^2 + bx + c
+def quadratic_equation(x):
+    a = 1  # Coefficient of x^2
+    b = -2  # Coefficient of x
+    c = 1  # Constant term
+    return a * x**2 + b * x + c
+
+# Generate x values
+x = np.linspace(-5, 5, 100)  # Creating 100 evenly spaced points between -5 and 5
+
+# Calculate y values for both linear and quadratic equations
+y_linear = linear_equation(x)
+y_quadratic = quadratic_equation(x)
+
+# Create the plot
+plt.figure(figsize=(10, 5))
+
+# Plot the linear equation
+plt.subplot(1, 2, 1)
+plt.plot(x, y_linear, label='y = 2x + 3', color='blue')
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('Linear Equation')
+plt.grid(True)
+plt.legend()
+
+# Plot the quadratic equation
+plt.subplot(1, 2, 2)
+plt.plot(x, y_quadratic, label='y = x^2 - 2x + 1', color='green')
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('Quadratic Equation')
+plt.grid(True)
+plt.legend()
+
+# Adjust layout for subplots
+plt.tight_layout()
+
+# Display the graph
+plt.show()
